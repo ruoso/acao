18e08dcc64bc6951cc727af2f8a217dbe0ffddda
Paulo Neto <pauloneto@pauloneto-laptop.(none)>
2010-03-15 17:02:50 -0300

    implementa a chamada dos plugins de coleta de dados.

--- Acao/lib/Acao/Model/ProcessoConsolidacao.pm
+++ Acao/lib/Acao/Model/ProcessoConsolidacao.pm
@@ -23,19 +23,56 @@ sub iniciar_consolidacao {
 	my $etapa = 1;
 	my @plugins_entrada = 
 		$consolidacao->definicao_consolidacao->etapa_coleta_dados->all;
+
+	# vamos incluir os "lib" dos plugins no nosso @INC
+	my $plugins_path = Acao->path_to('plugins');
+	opendir my $plugins_dir, $plugins_path;
+	while (my $subdir = readdir $plugins_dir) {
+		next if $subdir eq '.';
+		next if $subdir eq '..';
+		my $final = join '/', $plugins_path, $subdir, 'lib'; 
+		push @INC, $final;
+	}
+	closedir $plugins_dir;
+
 	foreach my $plugin_entrada (@plugins_entrada) {
 		my $classe = $plugin_entrada->plugin_coleta_dados;
-		if (eval "require $classe") {
-
-		} else {
+		eval "require $classe";
+		if ($@) {
+                        my $erro_original = $@;
 			$consolidacao->alertas->create
 				({ etapa => $etapa,
 				   log_level => 'FATAL',
 				   datahora => DateTime->now(),
-				   descricao_alerta => 'Erro carregando plugin - '.$@ });
-			die;
+				   descricao_alerta => 'Erro carregando plugin - '.$erro_original });
+			exit 1;
+		}
+
+		eval {
+			my $obj = $classe->new({ dbic => $self->dbic,
+						 user => $self->user,
+						 sedna => $self->sedna });
+ 			$obj->processar($consolidacao,'TODO');
+		};
+		if ($@) {
+                        my $erro_original = $@;
+			$consolidacao->alertas->create
+				({ etapa => $etapa,
+				   log_level => 'FATAL',
+				   datahora => DateTime->now(),
+				   descricao_alerta => 'Erro executando coleta de dados - '.$erro_original });
+			exit 1;
 		}
 	}
+
+	# inicia a etapa de validação.
+        $etapa = 2;
+
+	# vamos percorrer todos os documentos do collection de entrada
+ 	# dessa consolidacao e executar todos os plugins de validação para cada
+	# um deles.
+
+	
 
 }
 
