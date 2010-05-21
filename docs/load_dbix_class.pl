32ddcd6e4a03528313c77fba80107021df78947a
paulo neto <paulo@paulo-laptop.(none)>
2010-05-21 16:53:26 -0300

    comando para load do dbix::class

--- docs/load_dbix_class.pl
+++ docs/load_dbix_class.pl
@@ -1,6 +1,6 @@
 #importa as tabelas do acaodw e constroi as classe para o dbi class
 
-perl -MDBIx::Class::Schema::Loader=dump_to_dir:"/home/paulo/devel/acao-viladomar/trunk/schemas" -DBIx::Class::Schema::Loader -e 'DBIx::Class::Schema::Loader->connection("dbi:Pg:dbname=acaodw;host=127.0.0.1;port=5432","acao","blableblibloblu")'
+perl -MDBIx::Class::Schema::Loader=dump_to_dir:"/home/paulo/devel/acao-viladomar/trunk/schemas" -e 'DBIx::Class::Schema::Loader->connection("dbi:Pg:dbname=acaodw;host=127.0.0.1;port=5432","acao","blableblibloblu")'
 
 
 
