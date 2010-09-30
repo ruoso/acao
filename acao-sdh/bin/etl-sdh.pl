a7ec92f87e273338cb65f5bf47cd443b4da0d59e
joao_neto <joao_neto@ATLANTICO.(none)>
2010-09-30 11:56:16 -0300

    adicao de dimensoes no etl

--- acao-sdh/bin/etl-sdh.pl
+++ acao-sdh/bin/etl-sdh.pl
@@ -8,6 +8,7 @@ use lib catfile($Bin, '..', 'lib');
 use DateTime::Format::XSD;
 use strict;
 use warnings;
+use Data::Dumper;
 
 use aliased 'Acao::Plugins::SDH::DimSchema';
 
@@ -15,18 +16,29 @@ my $sedna = Sedna->connect('127.0.0.1', 'acao', 'acao', '12345');
 
 my $dbi = DimSchema->connect("dbi:Pg:dbname=sdh;host=127.0.0.1;port=5432",'acao','blableblibloblu');
 
+BEGIN {
+    die 'Informe a variavel de ambiente ACAO_HOME' unless -d $ENV{ACAO_HOME};
+}
+
+#define as constantes para os caminhos dos schemas, utilizando variável de ambiente
+use constant HOME_SCHEMAS => $ENV{HOME_SCHEMAS} || catfile($Bin, '..', 'schemas');
+use constant SCHEMA_IDENTIFICACAO_PESSOAL => catfile(HOME_SCHEMAS, 'sdh-identificacaoPessoal.xsd');
+
+use constant SCHEMA_CONSOLIDADO => catfile(HOME_SCHEMAS, 'sdh-consolidado.xsd');
+use constant SDH_CONSOLIDADO_NS =>"http://schemas.fortaleza.ce.gov.br/acao/sdh-consolidado.xsd";
+
+my $schema = XML::Compile::Schema->new(SCHEMA_CONSOLIDADO);
+my $read = $schema->compile(READER => pack_type(SDH_CONSOLIDADO_NS , 'conteudo'));
+
+
 sub extract {
 
  #   my ($id_volume) = @_;
 
     my $xq = 'declare namespace dos = "http://schemas.fortaleza.ce.gov.br/acao/dossie.xsd";
               declare namespace dc = "http://schemas.fortaleza.ce.gov.br/acao/documento.xsd";
-              let $sorted-people := 
-                    for $x in collection("volume-011BA75E-C712-11DF-8789-B79CE9E43239")/dos:dossie[dos:controle=1]/dos:doc/dc:documento 
-                        order by ($x/dc:criacao) descending  
-                    return $x/dc:documento/dc:conteudo/*
-                        [namespace-uri($x/dc:documento/dc:conteudo/*) = "http://schemas.fortaleza.ce.gov.br/acao/sdh-documentacaoFamiliar.xsd"]  
-                    for $x at $count in subsequence($sorted-people, 1,1) return $x';
+                    for $x in collection("volume-48F1B8CA-CAFF-11DF-9F68-C6003924FB0B")/dos:dossie[dos:controle="4A982326-CAFF-11DF-9F68-C6003924FB0B"]
+                    /dos:doc/dc:documento[dc:invalidacao/text() eq "1970-01-01T00:00:00Z"]/dc:documento/* return $x';
 
     #inicia a conexão com o sedna
     $sedna->begin;
@@ -39,14 +51,123 @@ sub extract {
         my $xml_string = $sedna->getItem();
   }
     $sedna->commit;
+    
 }
 
 sub transform {
+    my ($data) = @_;
+    transform_endereco($data->{formIdentificacaoPessoal}{endereco});
+}
 
+sub transform_endereco {
+    my $data = shift;
+    $data->{endereco} = $dbi->resultset('DEndereco')->find_or_create({ endereco => $data->{endereco},
+                                                                       bairro => $data->{bairro},
+                                                                       cidade => $data->{cidade},
+                                                                     })->id_endereco;
+}
+
+sub transform_estado_civil {
+    my $data = shift;
+    $data->{estado_civil} = $dbi->resultset('DEstadoCivil')->find_or_create({ estado_civil => $data->{estado_civil},
+                                                                            })->id_estado_civil;
+}
+
+sub transform_raca_etnia {
+    my $data = shift;
+    $data->{raca_etnia} = $dbi->resultset('DRacaEtinia')->find_or_create({ raca_etnia => $data->{raca_etnia},
+                                                                            })->id_estado_civil;
+}
+
+sub transform_sexo {
+    my $data = shift;
+    $data->{sexo} = $dbi->resultset('DSexo')->find_or_create({ raca_etnia => $data->{sexo},
+                                                                            })->id_sexo;
+}
+
+sub transform_sexualidade {
+    my $data = shift;
+    $data->{sexualidade} = $dbi->resultset('DSexualidade')->find_or_create({ raca_etnia => $data->{sexualidade},
+                                                                            })->id_sexualidade;
+}
+
+sub transform_registro_nascimento {
+    my $data = shift;
+    $data->{registro_nascimento} = $dbi->resultset('DRegistroNascimento')->find_or_create({ situacao => $data->{registro_nascimento},
+                                                                            })->id_registro_nascimento;
+}
+
+sub transform_identidade {
+    my $data = shift;
+    $data->{identidade} = $dbi->resultset('DIdentidade')->find_or_create({ situacao => $data->{identidade},
+                                                                            })->id_identidade;
+}
+
+sub transform_cpf {
+    my $data = shift;
+    $data->{cpf} = $dbi->resultset('DCpf')->find_or_create({ situacao => $data->{cpf},
+                                                                            })->id_cpf;
+}
+
+sub transform_ctps {
+    my $data = shift;
+    $data->{ctps} = $dbi->resultset('DCtps')->find_or_create({ situacao => $data->{ctps},
+                                                                            })->id_ctps;
+}
+
+sub transform_titulo_eleitor {
+    my $data = shift;
+    $data->{titulo_eleitor} = $dbi->resultset('DTitulo_eleitor')->find_or_create({ situacao => $data->{titulo_eleitor},
+                                                                            })->id_titulo_eleitor;
+}
+
+sub transform_reservista {
+    my $data = shift;
+    $data->{reservista} = $dbi->resultset('DReservista')->find_or_create({ situacao => $data->{reservista},
+                                                                            })->id_reservista;
+}
+
+sub transform_nis {
+    my $data = shift;
+    $data->{nis} = $dbi->resultset('DNis')->find_or_create({ situacao => $data->{nis},
+                                                                            })->id_nis;
+}
+
+sub transform_data {
+    my $data = shift;
+    my $dt = DateTime::Format::XSD->parse_datetime( $data->{data} );
+    $data->{data} = $dbi->resultset('DData')->find_or_create({ data => $data->{data},
+                                                               dia  => $dt->day,
+                                                               mes  => $dt->month,
+                                                               ano  => $dt->year,
+                                                               bimestre => int(($dt->month-1)/2)+1,
+                                                               trimestre => int(($dt->month-1)/3)+1,
+                                                               semestre => $dt->month < 6 ? 1 : 2,
+                                                               dia_semana => $dt->day_of_week,
+                                                              })->id_data;
 }
 
 sub load{
- $dbi->resultset('DAtendimentoadolescentesatividade')->create({raca => "preto tissão"})
+    my ($data) = @_;
+     $dbi->resultset('FAtendimento')
+      ->create({
+              id_endereco => $data->{endereco},
+              id_estado_civil => $data->{estado_civil},
+              id_raca_etnia => $data->{raca_etnia},
+              id_sexo => $data->{sexo},
+              id_sexualidade => $data->{sexualidade},
+              id_data => $data->{data},
+              id_idade => $data->{idade},
+              id_endereco => $data->{endereco},
+              deficiencia_sensorial_surdo =>$data->{deficiencia_sensorial_surd},
+              deficiencia_sensorial_cego =>$data->{deficiencia_sensorial_cego},
+              deficiencia_fisico_motor =>$data->{deficiencia_fisico_motor},
+              deficiencia_mobilidade_reduzida =>$data->{deficiencia_mobilidade_reduzida},
+              deficiencia_intelectual =>$data->{deficiencia_intelectual},
+     })
 }
- extract();
- load();
+
+
+extract();
+
+# load();
