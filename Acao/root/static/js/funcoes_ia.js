8e0194f8da1b5484d6c0a08e2651831fef5b8e22
jackson_gomes <jackson_gomes@ATLANTICO2688.(none)>
2010-11-30 11:30:33 -0300

    novos arquivos e plugins

---
+++ Acao/root/static/js/funcoes_ia.js
@@ -0,0 +1,69 @@
+jQuery(document).ready(function() {
+
+//http://tablesorter.com/
+//$("#volumetable").tablesorter();
+
+
+//grid.columnsizing
+try{
+alert($("#volumetable").columnSizing(
+	{
+				viewResize : true, 
+				viewGhost : true
+				}
+);
+}catch(e){alert(e)};
+
+/*
+try{
+$("#volumetable")
+			.columnSizing({
+				viewResize : true, 
+				viewGhost : true
+				})
+			.end();
+}catch(e){alert(e)};
+
+
+/*$("#volumetable").columnSizing({
+				viewResize : false, 
+				viewGhost : false, 
+				selectCells : "tr:first>*:not(:first)"
+				})
+			.end()
+*/
+
+
+
+/*Ingrid
+	$("#volumetable").ingrid({ 
+			height: 350,
+
+sorting: false,
+			paging: true
+		});
+
+			/*Usando FlexiGrid
+			$("#volumetable").flexigrid
+			(
+			{
+			colModel : [
+				{display: 'Volume', name : 'Volume', align: 'center'},
+				{display: 'Criação', name : 'Criação', align: 'left'},
+
+				{display: 'Fechamento', name : 'Fechamento', hide: false},
+				{display: 'Arquivamento', name : 'Arquivamento', sortable : true, align: 'right'},
+				{display: 'Estado', name : 'Estado', sortable : false, align: 'right'},
+				{display: 'Classificação', name : 'Classificação', sortable : false, align: 'right'},
+				{display: 'Localização', name : 'Localização', sortable : false, align: 'right'},
+				{display: 'Vol. Físico', name : 'Vol. Físico', sortable : false, align: 'right'},
+				{display: 'Ação', name : 'Ação', sortable : false, align: 'right'},
+
+				]
+			}
+			);		
+		*/
+
+									
+
+});
