fb5d22d0be386b482ae2509441f2ad6386616378
Carlos Wherbet <carloswherbet@gmail.com>
2011-03-02 11:20:42 -0300

    Administração::Busca de schemas xsd

--- Acao/lib/Acao/Model/Schema.pm
+++ Acao/lib/Acao/Model/Schema.pm
@@ -19,9 +19,8 @@ sub listar_schemas {
     my $list =
 'declare namespace class = "http://schemas.fortaleza.ce.gov.br/acao/classificacao.xsd";'
       . 'for $x in collection("acao-schemas")/xs:schema'
-      . ' return ('
+      . ' return ($x,'
       . $args->{grid} . '  )';
-
 
     return {
         list => $list
@@ -31,20 +30,51 @@ sub listar_schemas {
 }
 
 sub buscar_schemas {
-    my ( $self, $filtro,$template ) = @_;
+    my ( $self, $template, $filtro ) = @_;
     my @return;
 
     my $list =
 'declare namespace class = "http://schemas.fortaleza.ce.gov.br/acao/classificacao.xsd";'
-      . 'for $x in collection("acao-schemas")/xs:schema'
+      . 'for $x in collection("acao-schemas")/xs:schema '
       . 'where $x/xs:element/xs:annotation/xs:appinfo/class:classificacoes/class:classificacao/text() = '
-      . $filtro
-      . ' return ($x)';
+      . '"'
+      . $filtro . '"'
+      . ' return ($x, '
+      . $template . ')';
 
-    return {
-        list => $list
-    };
+    return { list => $list };
 }
 
+sub options_classificacao_xsd {
+    my ($self) = @_;
+
+    my $query =
+'declare namespace class = "http://schemas.fortaleza.ce.gov.br/acao/classificacao.xsd";'
+      . 'for $x in collection("acao-schemas")/xs:schema '
+      . 'order by $x/xs:element/xs:annotation/xs:appinfo/class:classificacoes/class:classificacao/text()'
+      . ' return     '
+      . '(<option value="{$x/xs:element/xs:annotation/xs:appinfo/class:classificacoes/class:classificacao/text()}">'
+      . '{replace($x/xs:element/xs:annotation/xs:appinfo/class:classificacoes/class:classificacao/text(),"cn=","")}'
+      . '</option>)';
+
+
+    $self->sedna->begin();
+    $self->sedna->execute($query);
+    my $options;
+    my %anterior;
+    while ( my $item = $self->sedna->get_item() ) {
+        $item =~ s/^\s+//go;
+        unless ( $anterior{$item} ) {
+            $options .= $item;
+            $anterior{$item} = 1;
+        }
+    }
+
+
+    $self->sedna->commit;
+
+    return $options;
+
+}
 1;
 
