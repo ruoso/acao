6989081f37293c9ee94ba0f89c06e534e609ed7e
Carlos Wherbet <carloswherbet@gmail.com>
2011-03-17 11:01:59 -0300

    Listagem de Usuários e Inserção de Usuários no LDAP. Modificações e Adições no acao.conf e Ajustes de css. disponibilizados mais icones ao sistema.

--- Acao/lib/Acao/Controller/Auth/Admin/Usuario.pm
+++ Acao/lib/Acao/Controller/Auth/Admin/Usuario.pm
@@ -23,20 +23,153 @@ use namespace::autoclean;
 BEGIN { extends 'Catalyst::Controller'; }
 use Data::Dumper;
 use utf8;
+use XML::Simple;
 
-sub base : Chained('/auth/admin/base') : PathPart('usuarios') : CaptureArgs(0)
-{
-     my ( $self, $c ) = @_;
+#with 'Acao::Role::Controller::Requisicao';
+
+sub base : Chained('/auth/admin/base') : PathPart('usuario') : CaptureArgs(0) {
+    my ( $self, $c ) = @_;
 }
 
+sub getUsuario : Chained('base') : PathPart('') : CaptureArgs(1) {
+    my ( $self, $c, $dn_usuario ) = @_;
+    $c->stash->{dn_usuario} = $dn_usuario;
+}
 
 sub lista : Chained('base') : PathPart('') : Args(0) {
-    my ($self, $c ) = @_;
+    my ( $self, $c ) = @_;
     my @usuarios = $c->model('Usuario')->buscar_usuarios();
-      $c->stash->{usuarios} = \@usuarios;
+    $c->stash->{usuarios} = \@usuarios;
 
     return;
 }
 
+sub adicionar_usuario : Chained('base') : PathPart('cadastrar') : Args(0) {
+    my ( $self, $c ) = @_;
+    $c->stash->{lotacao} = '<opt> </opt>';
+
+}
+
+sub navega_ldap : Chained('base') : PathPart('navega_ldap') : Args(0) {
+    my ( $self, $c ) = @_;
+    $c->stash->{template}     = 'auth/admin/usuario/navega_ldap.tt';
+    $c->stash->{basedn}       = $c->req->param('grupos');
+    $c->stash->{autorizacoes} = $c->req->param('autorizacoes');
+    return 1;
+
+}
+
+sub ver_user : Chained('getUsuario') : PathPart('') : Args(0) {
+    my ( $self, $c ) = @_;
+    $c->stash->{template} = 'auth/admin/usuario/usuario.tt';
+    $c->stash->{usuario}  =
+      $c->model('Usuario')->getDadosUsuarioLdap( $c->stash->{dn_usuario} );
+    warn Dumper( $c->stash->{usuario} );
+
+}
+
+sub alterar : Chained('getUsuario') : PathPart('alterar') : Args(0) {
+    my ( $self, $c ) = @_;
+
+}
+
+sub store : Chained('base') : PathPart('store') : Args(0) {
+    my ( $self, $c ) = @_;
+    my $result;
+    my $volumeArray    = [ $c->req->param('volume[]') ];
+    my $dossieArray    = [ $c->req->param('dossie[]') ];
+    my $documentoArray = [ $c->req->param('documento[]') ];
+    my $lotacaoArray   = desserialize( $c->req->param('lotacao') )->{value};
+    if ( ref($lotacaoArray) ne 'ARRAY' ) {
+        $lotacaoArray = [ desserialize( $c->req->param('lotacao') )->{value} ];
+    }
+    my $super = $c->req->param('super');
+
+
+
+    eval {
+        $result = $c->model('Usuario')->storeUsuario(
+            {
+                'senha'     => $c->req->param('senha'),
+                'id'        => $c->req->param('uid'),
+                'nome'      => $c->req->param('nome'),
+                'sobrenome' => $c->req->param('sobrenome'),
+                'email'     => $c->req->param('email'),
+                'fone'      => $c->req->param('fone'),
+                'dominio'   => $c->req->param('dominio'),
+                'volume'    => $volumeArray,
+                'dossie'    => $dossieArray,
+                'documento' => $documentoArray,
+                'lotacao'   => $lotacaoArray,
+                'super'   => $super,
+            }
+        );
+    };
+    warn Dumper($result);
+
+    if ( $result->{resultCode} ne '0' ) {
+        $c->flash->{erro} = 'ldap-' .$result->{resultCode};
+        $c->res->redirect(
+            $c->uri_for_action('/auth/admin/usuario/adicionar_usuario') );
+        return;
+    }
+    else {
+        $c->flash->{sucesso} = 'Adicionado';
+
+    }
+    $c->res->redirect( $c->uri_for_action('/auth/admin/usuario/lista') );
+
+}
+
+sub add : Chained('base') : PathPart('add') : Args(0) {
+    my ( $self, $c, ) = @_;
+    $c->stash->{template} = 'auth/admin/usuario/grid_lotacao.tt';
+    my @principal = split /-/, $c->req->param('grupos');
+    my $hash_old = desserialize( $c->req->param('lotacao') );
+    if ( scalar keys %$hash_old == 0 ) {
+        $hash_old = { value => [] };
+    }
+    if ( ref( $hash_old->{value} ) ne 'ARRAY' ) {
+        $hash_old = { value => [ $hash_old->{value} ] };
+    }
+    my $hash   = { value => [ map { $_ } @principal ] };
+    my $arrayA = $hash_old->{value};
+    my $arrayB = $hash->{value};
+    push( @$arrayA, @$arrayB );
+    my $lotacao = serialize($hash_old);
+    $c->stash->{lotacao}  = $lotacao;
+    $c->stash->{lotacoes} = { value => $arrayA };
+
+}
+
+sub desserialize {
+    my ($xml_) = @_;
+    my $xml = new XML::Simple;
+    return $xml->XMLin($xml_);
+}
+
+sub serialize {
+    my ($hash) = @_;
+    my $xml = new XML::Simple;
+    return $xml->XMLout($hash);
+
+}
+
+sub remove : Chained('base') : PathPart('remover') : Args(0) {
+    my ( $self, $c ) = @_;
+    $c->stash->{template} = 'auth/admin/usuario/grid_lotacao.tt';
+    my $hash  = desserialize( $c->req->param('xmllotacao') );
+    my $array = $hash->{value};
+
+    if ( scalar($array) <= 1 ) {
+        $array = [];
+    }
+    delete $array->[ $c->req->param('posicao') ];
+    join ',', splice @$array, $c->req->param('posicao'), 1;
+    my $lotacao = serialize( { value => [@$array] } );
+    $c->stash->{lotacao}  = $lotacao;
+    $c->stash->{lotacoes} = { value => [@$array] };
+
+}
 
 1;
--- Acao/lib/Acao/Model/Usuario.pm
+++ Acao/lib/Acao/Model/Usuario.pm
@@ -4,11 +4,11 @@ use Moose;
 use Data::Dumper;
 use strict;
 use warnings;
+use Digest::SHA qw(sha1_base64 sha1);
 use List::MoreUtils qw{uniq};
 
 use Carp qw(croak);
 extends 'Acao::Model::LDAP';
-
 
 sub buscar_usuarios_ {
     my ($self) = @_;
@@ -47,17 +47,104 @@ sub buscar_usuarios {
     }
 
     foreach my $dn ( uniq @dn_s ) {
+        my $dn_user = $dn;
         $dn =~ s/uid=//go;
         @nome = split /,/, $dn;
         push @usuarios,
           {
             nome => $nome[0],
-            dn   => $dn
+            dn   => $dn_user
           };
     }
 
     croak 'LDAP error: ' . $mesg->error if $mesg->is_error;
     return @usuarios;
+}
+
+sub getDadosUsuarioLdap {
+    my ( $self, $dn ) = @_;
+    my $mesg = $self->ldap->search(
+        base   => $self->dominios_dn,
+        filter => "(&(entryDN=" . $dn . "))",
+        scope  => 'sub',
+        attrs  =>
+          [qw/cn sn uid givenName email mobile telephoneNumber memberOf/],
+    );
+
+    return {
+        uid       => $mesg->entry->get_value('uid'),
+        nome      => $mesg->entry->get_value('cn'),
+        sobrenome => $mesg->entry->get_value('sn'),
+        apelido   => $mesg->entry->get_value('givenName'),
+        email     => [ $mesg->entry->get_value('email') ],
+        celular   => [ $mesg->entry->get_value('mobile') ],
+        fone      => $mesg->entry->get_value('telephoneNumber'),
+        dn        => $dn,
+        memberOf  => [ $mesg->entry->get_value('memberOf') ],
+    };
+
+}
+
+sub storeUsuario {
+    my ( $self, $args ) = @_;
+    my $host     = $self->ldap_config->{host};
+    my $DNbranch = $args->{dominio};
+    my $senha    = sha1_base64( $args->{senha} );
+    $senha .= '=' while ( length($senha) % 4 );
+
+    my $createArray = [
+        objectClass => [
+            qw/top person organizationalPerson
+              inetOrgPerson recursoHumano simpleSecurityObject
+              account/
+        ],
+        cn              => $args->{nome},
+        uid             => $args->{id},
+        sn              => $args->{sobrenome},
+        givenName       => $args->{nome},
+        email           => $args->{email},
+        telephoneNumber => $args->{fone},
+        userPassword    => '{SHA}' . $senha,
+
+    ];
+
+    my $volumeArray    = $args->{volume};
+    my $dossieArray    = $args->{dossie};
+    my $documentoArray = $args->{documento};
+    my $lotacaoArray   = $args->{lotacao};
+    my $super          = $args->{super};
+
+    my $NewDN = @$createArray[4] . '=' . @$createArray[5] . ',' . $DNbranch;
+    my $result = $self->LDAPentryCreate( $NewDN, $createArray );
+
+    if ($result->{resultCode} ne '0') { return $result; }
+
+
+    foreach my $acao (@$volumeArray) {
+        $self->LDAPInsertMemberEntry(
+            Acao->config->{'roles'}->{'volume'}->{$acao}, $NewDN );
+    }
+    foreach my $acao (@$dossieArray) {
+        $self->LDAPInsertMemberEntry(
+            Acao->config->{'roles'}->{'dossie'}->{$acao}, $NewDN );
+    }
+    foreach my $acao (@$documentoArray) {
+        $self->LDAPInsertMemberEntry(
+            Acao->config->{'roles'}->{'documento'}->{$acao}, $NewDN );
+    }
+    foreach my $lotacao (@$lotacaoArray) {
+        $self->LDAPInsertMemberEntry( $lotacao, $NewDN );
+    }
+
+    if ($super) {
+        $self->LDAPInsertMemberEntry(
+            Acao->config->{'Model::LDAP'}->{'admin_super'}, $NewDN ) ;
+    }
+
+    return $result;
+
+    #
+
 }
 
 1;
